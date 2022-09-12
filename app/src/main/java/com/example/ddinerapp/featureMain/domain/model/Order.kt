0167fb1550e35f777c572a7ee282f3cca6bc7944
lucasnpc <lucasnpc2@outlook.com>
2022-09-12 00:06:38 -0300

    Starting order

--- app/src/main/java/com/example/ddinerapp/featureMain/domain/model/Order.kt
+++ app/src/main/java/com/example/ddinerapp/featureMain/domain/model/Order.kt
@@ -1,11 +1,9 @@
 package com.example.ddinerapp.featureMain.domain.model
 
-import java.sql.Timestamp
-
 data class Order(
     val id: String = "",
     val concluded: Boolean = false,
     val employeeCpf: String = "",
-    val startDate: Timestamp = Timestamp(System.currentTimeMillis()),
-    val endDate: Timestamp? = null
+    val startDate: String = "",
+    val endDate: String? = null
 )
--- app/src/main/java/com/example/ddinerapp/featureMain/domain/useCases/SetOccupiedDeskUseCase.kt
+++ app/src/main/java/com/example/ddinerapp/featureMain/domain/useCases/SetOccupiedDeskUseCase.kt
@@ -2,9 +2,13 @@ package com.example.ddinerapp.featureMain.domain.useCases
 
 import com.example.ddinerapp.common.util.BUSINESS_COLLECTION
 import com.example.ddinerapp.common.util.DESKS_COLLECTION
+import com.example.ddinerapp.common.util.ORDERS_COLLECTION
 import com.example.ddinerapp.featureMain.domain.model.Desk
+import com.example.ddinerapp.featureMain.domain.model.Order
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SetOccupiedDeskUseCase {
     private val db = Firebase.firestore
@@ -14,5 +18,21 @@ class SetOccupiedDeskUseCase {
             db.collection(BUSINESS_COLLECTION).document(cnpj).collection(DESKS_COLLECTION)
                 .document(desk.id)
         document.update("isOccupied", true)
+
+        val simpleDateFormat =
+            SimpleDateFormat("dd 'de' MMMM 'de' yyyy HH:mm:ss", Locale.getDefault())
+
+        Order(startDate = simpleDateFormat.format(System.currentTimeMillis())).let {
+            document.collection(ORDERS_COLLECTION)
+                .add(
+                    hashMapOf(
+                        "conclude" to it.concluded,
+                        "employeeCpf" to it.employeeCpf,
+                        "startDate" to it.startDate,
+                        "endDate" to it.endDate
+                    )
+                )
+        }
+
     }
 }
